d4a90605e9e08fb9547e57a256eceb2bb021497f
John Caldwell <john.caldwell@caldwelldevelopment.com>
2025-12-26 16:06:34 -0500

    Create two functions - one is for downloading videos from YouTube and the other is for downloading audio only from YouTube.

--- youtube_downloader_python.py
+++ youtube_downloader_python.py
@@ -4,15 +4,16 @@ from pytubefix.cli import on_progress
 from pytubefix import exceptions
 import os
 
-def youtube_video_download(url):
+def youtube_video_download(url, path):
     try:
         yt = YouTube(url, on_progress_callback = on_progress)
         print(yt.title)
         ys = yt.streams.get_highest_resolution()
-        ys.download()
+        ys.download(output_path=path)
         print("\nDone")
     except exceptions.VideoUnavailable:
         print("Try a different URL...")
+        main()
 
 def youtube_audio_download():
     url = ""
@@ -25,8 +26,14 @@ def youtube_audio_download():
     os.rename(out_file, new_file)
     print("\nDone!")
 
+def main():
+       
+    url = input('Enter url: ')
+    path = input('Which do you want this file saved: ')
+
+    youtube_video_download(url, path)
+
 if __name__ == "__main__":
 
-    url = input('Enter url: ')
+    main()
 
-    youtube_video_download(url)
