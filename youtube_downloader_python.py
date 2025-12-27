db127e583511ab709d8f6a7ee3e2f8e74f164749
John Caldwell <john.caldwell@caldwelldevelopment.com>
2025-12-27 07:57:24 -0500

    Added try and except on audio only function. The function takes in a string for url and path to save the file.

--- youtube_downloader_python.py
+++ youtube_downloader_python.py
@@ -4,7 +4,7 @@ from pytubefix.cli import on_progress
 from pytubefix import exceptions
 import os
 
-def youtube_video_download(url, path):
+def youtube_video_download(url: str, path: str):
     try:
         yt = YouTube(url, on_progress_callback = on_progress)
         print(yt.title)
@@ -13,25 +13,28 @@ def youtube_video_download(url, path):
         print("\nDone")
     except exceptions.VideoUnavailable:
         print("Try a different URL...")
-        main()
+        
 
-def youtube_audio_download():
-    url = ""
-    yt = YouTube(url, on_progress_callback = on_progress)
-    print(yt.title)
-    ys = yt.streams.filter(only_audio=True).first()
-    out_file = ys.download(output_path='.')
-    base, ext = os.path.splitext(out_file)
-    new_file = base + '.mp3'
-    os.rename(out_file, new_file)
-    print("\nDone!")
+def youtube_audio_download(url: str, base: str):
+    try:
+        yt = YouTube(url, on_progress_callback = on_progress)
+        print(yt.title)
+        ys = yt.streams.filter(only_audio=True).first()
+        out_file = ys.download(output_path='.')
+        new_file = f'{base}\{yt.title}.mp3'
+        print(new_file)
+        os.rename(out_file, new_file)
+        print("\nDone!")
+    except FileExistsError:
+        print(f'File already exist. {base}')
 
 def main():
        
     url = input('Enter url: ')
-    path = input('Which do you want this file saved: ')
+    # path = input('Which do you want this file saved: ')
+    base = input('Where to save audio file to: ')
 
-    youtube_video_download(url, path)
+    youtube_audio_download(url, base)
 
 if __name__ == "__main__":
 
