7311b743604cfb8c9ee46d39c12ed91acb43f81a
John Caldwell <john.caldwell@caldwelldevelopment.com>
2025-12-29 08:24:55 -0500

    Fix error - button click was getting empty strings. Redid the code to fix empty strings

--- youtube_interface.py
+++ youtube_interface.py
@@ -1,20 +1,52 @@
 import customtkinter
 import youtube_downloader
 
-resolution = "600x400"
+resolution = "600x300"
 
 window = customtkinter.CTk()
 window.geometry(resolution)
 font_title = customtkinter.CTkFont(family="Comic Sans MS", size=30)
 
-customtkinter.CTkLabel(window, text="YouTube Downloader", font=font_title, text_color="red").place(x=180, y=15)
+customtkinter.CTkLabel(
+    window,
+    text="YouTube Downloader",
+    font=font_title,
+    text_color="red"
+).place(x=180, y=15)
 
 customtkinter.CTkLabel(window, text="Enter the YouTube URL: ").place(x=20, y=100)
-youtube_url = customtkinter.CTkEntry(window, width=380, height=25)
-youtube_url.place(x=160, y=100)
+txtyoutube_url = customtkinter.CTkEntry(window, width=380, height=25)
+txtyoutube_url.place(x=160, y=100)
 
 customtkinter.CTkLabel(window, text="Where to save video or audio: ").place(x=20, y=150)
-save_location = customtkinter.CTkEntry(window, width=350, height=25)
-save_location.place(x=190, y=150)
+txtsave_location = customtkinter.CTkEntry(window, width=350, height=25)
+txtsave_location.place(x=190, y=150)
+
+customtkinter.CTkLabel(window, text="How do you want to save the download: ").place(x=20, y=200)
+
+cboselection = customtkinter.CTkComboBox(
+    window,
+    values=["Video", "Audio"]
+)
+cboselection.set("Video")  # 👈 default selection
+cboselection.place(x=50, y=250)
+
+def download():
+    selection = cboselection.get().lower()
+    youtube_url = txtyoutube_url.get()
+    save_location = txtsave_location.get()
+
+    youtube_downloader.select_video_or_audio(
+        selection,
+        youtube_url,
+        save_location
+    )
+
+btndownload = customtkinter.CTkButton(
+    window,
+    text="Download Now",
+    command=download
+)
+btndownload.place(x=300, y=225)
 
 window.mainloop()
